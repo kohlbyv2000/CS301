4e4c0971b02bd04d17a05a1881e085a9e34a54f7
kohlbyv2000 <kohlbyv2000@gmail.com>
2024-11-23 08:53:06 +0000

    Set up fltk

--- main.cpp
+++ main.cpp
@@ -3,9 +3,20 @@
 // 2024/11/19
 // Evil Autocorrect main implementation
 
-#include "words.hpp"
+//#include "words.hpp"
+#include "fltk/FL/Fl.H"
+#include "fltk/FL/Fl_Window.H"
+#include "fltk/FL/Fl_Button.H"
+
+void button_callback(Fl_Widget* widget, void*) {
+    Fl::warning("Button Clicked!");
+}
 
 int main() {
-    
-    return 0;
+    Fl_Window *window = new Fl_Window(400, 300, "My FLTK Application");
+    Fl_Button *button = new Fl_Button(160, 120, 80, 40, "Click Me");
+    button->callback(button_callback);
+    window->end();
+    window->show();
+    return Fl::run();
 }
